3d2c9e9ff0f9725189457257141a65b6604e5d33
dishant <dishantkumar1011@gmail.com>
2025-10-06 09:19:32 +0530

    candidateTimeline

--- src/components/CandidateTimeline.jsx
+++ src/components/CandidateTimeline.jsx
@@ -1,3 +1,4 @@
+// src/components/CandidateTimeline.jsx
 import React from 'react';
 
 const CandidateTimeline = ({ timeline }) => {
@@ -6,7 +7,10 @@ const CandidateTimeline = ({ timeline }) => {
             {/* Vertical Line */}
             <div className="absolute left-0 top-0 h-full w-0.5 bg-gray-300"></div>
 
-            {timeline.slice().reverse().map((item, index) => (
+            {(timeline || []) // <--- FIX: Ensure 'timeline' is an array before calling .slice()
+                .slice()
+                .reverse()
+                .map((item, index) => (
                 <div key={index} className="mb-6 relative">
                     {/* Circle/Dot */}
                     <div className="absolute left-0 top-0 h-4 w-4 rounded-full bg-indigo-600 -ml-2.5 border-4 border-white"></div>
